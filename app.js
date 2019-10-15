1271be1fd39f62fe9b16d445c97029e34afbffca
OlegShynkarenko <oleg.shynkarenko1985@gmail.com>
2019-10-15 14:14:03 +0300

    setup local strategy for the passport.js

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import cookieParser from 'cookie-parser';
+import passport from "passport";
+import flash from "express-flash";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+
 import { connectDb } from "./models";
 import registerUser from "./routes/registerUser";
+import logInUser  from "./routes/logInUser"
+import initializePassport from "./passport-config";
 
 const app = express();
+initializePassport(passport);
 
 app.use(cors());
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use("/api/register-user", registerUser);
+app.use("/api/register", registerUser);
 app.use("/api/get-users", registerUser);
+app.use("/api/login", logInUser);
 
-connectDb().then( () => {
-  app.listen(process.env.PORT, () =>
-    console.log(`App listening on port ${process.env.PORT}!`)
-  );
-}).catch((e) => console.log('Something went wrong...', e));
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`App listening on port ${process.env.PORT}!`)
+    );
+  })
+  .catch(e => console.log("Something went wrong...", e));
 
 module.exports = app;
--- routes/registerUser.js
+++ routes/registerUser.js
@@ -6,17 +6,21 @@ import models from "../models";
 const router = Router();
 
 router.post('/', async (req, res) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const user = models.User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    birthDate: req.body.birthDate,
-    country: req.body.country,
-    email: req.body.email,
-    password: hashedPassword
-  });
-
-  return res.send(user)
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await models.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthDate: req.body.birthDate,
+      country: req.body.country,
+      email: req.body.email,
+      password: hashedPassword
+    });
+    res.send('/login')
+  } catch(e) {
+    console.log(e);
+    res.send('/register')
+  }
 });
 
 router.get('/', async (req, res) => {
