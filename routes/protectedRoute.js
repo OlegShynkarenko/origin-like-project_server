68fd001549d49d6ffe956e935f5a261638da7c52
OlegShynkarenko <oleg.shynkarenko1985@gmail.com>
2019-11-02 13:40:40 +0200

    add mongoDB Atlas

--- routes/protectedRoute.js
+++ routes/protectedRoute.js
@@ -3,6 +3,7 @@ import {Router} from "express";
 const router = Router();
 
 router.get('/', (req, res) => {
+  console.log(req.sessionID)
   console.log('Inside GET /auth-required callback');
   console.log(`User authenticated? ${req.isAuthenticated()}`);
   if(req.isAuthenticated()) {
