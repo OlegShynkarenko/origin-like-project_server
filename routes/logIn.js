25411e5f875188c5beb2a7ea5bf106fbe2ea429f
Oleg Shynkarenko <oleg.shynkarenko1985@gmail.com>
2021-10-20 16:09:51 +0300

    Update logIn.js

--- routes/logIn.js
+++ routes/logIn.js
@@ -5,14 +5,10 @@ import {ErrorHandler} from "../error";
 const router = Router();
 
 router.get('/', (req, res) => {
-  console.log('Inside GET /login callback function');
-  console.log(req.sessionID);
   res.send(`You got the login page!\n`)
 });
 
 router.post('/', (req, res, next) => {
-  console.log('Inside POST /login callback')
-  console.log('Post callback', req.sessionID)
   passport.authenticate('local',{session: true}, (err, user, info) => {
     try {
       if(info) throw new ErrorHandler(401, info.message);
